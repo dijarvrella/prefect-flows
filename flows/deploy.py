225e176b3355d4a4dcdd430b3463d5a280567f72
Dijar Vrella <dijar.vrella@regionstockholm.se>
2024-06-16 21:23:03 +0200

    chore: Update deploy.py to handle deployment errors

--- flows/deploy.py
+++ flows/deploy.py
@@ -3,16 +3,20 @@ from prefect import flow
 from prefect.runner.storage import GitRepository
 from prefect_github import GitHubCredentials
 
-
 if __name__ == "__main__":
-    flow.from_source(
-        source=GitRepository(
-        url="https://github.com/dijarvrella/prefect-flows",
-        credentials=GitHubCredentials.load("dv")
-    ),  
-    entrypoint="prefect-flows-main/flows/create_container.py:create_docker_container_flow",
-    ).deploy(
-        name="create-container-from-git-deployment",
-        work_pool_name="seedoo-container-worker",
-        build=False
-    )
+    print("Starting to deploy flow from source...")
+    try:
+        flow.from_source(
+            source=GitRepository(
+                url="https://github.com/dijarvrella/prefect-flows",
+                credentials=GitHubCredentials.load("dv")
+            ),
+            entrypoint="prefect-flows-main/flows/create_container.py:create_docker_container_flow",
+        ).deploy(
+            name="create-container-from-git-deployment",
+            work_pool_name="seedoo-container-worker",
+            build=False
+        )
+        print("Flow successfully deployed.")
+    except Exception as e:
+        print(f"Error occurred: {e}")
