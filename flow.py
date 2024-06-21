a391c14f8a9779e4b6ed0e4f85eaf14dab07a373
Dijar Vrella <dijar.vrella@regionstockholm.se>
2024-06-21 16:58:11 +0200

    chore: Update flow.py to pull and run nginx from private Docker registry

--- flow.py
+++ flow.py
@@ -1,25 +1,29 @@
 import asyncio
-from prefect import flow
+from prefect import flow, task, get_run_logger
 from prefect_docker.images import pull_docker_image
-from prefect_docker.containers import create_docker_container, start_docker_container
+from prefect_docker.containers import create_docker_container, start_docker_container, get_docker_container_logs
 from prefect_docker.credentials import DockerRegistryCredentials
 
-@flow
-async def pull_and_run_image():
-    # Load Docker credentials
-    docker_registry_credentials = await DockerRegistryCredentials.load("seedoo-docker-registry")
-
+@task
+async def pull_image(credentials):
+    logger = get_run_logger()
     # Pull the latest image from the private repository
+    logger.info("Pulling the latest image from the private repository...")
     image = await pull_docker_image(
         repository="seedooinsights/build",
-        tag="11ba042",
-        docker_registry_credentials=docker_registry_credentials
+        tag="2f87053",
+        docker_registry_credentials=credentials
     )
-    print(f"Pulled image: {image}")
+    logger.info(f"Pulled image: {image}")
+    return image
 
+@task
+async def create_container():
+    logger = get_run_logger()
     # Create a container with the specified flags
+    logger.info("Creating a container with the specified flags...")
     container = await create_docker_container(
-        image="seedooinsights/build:11ba042",
+        image="seedooinsights/build:2f87053",
         command=["python", "test.py"],
         volumes=[
             "/seedoodata:/seedoodata",
@@ -37,11 +41,50 @@ async def pull_and_run_image():
         stdin_open=True,  # -it is a combination of -i (interactive) and -t (tty)
         cap_add=["SYSLOG"]
     )
-    print(f"Created container: {container}")
+    logger.info(f"Created container: {container}")
+    return container
+
+@task
+async def start_container(container_id):
+    # Start the container
+    logger = get_run_logger()
+    logger.info(f"Starting container with ID: {container_id}...")
+    started_container = await start_docker_container(container_id=container_id)
+    logger.info(f"Started container: {started_container}")
+    return started_container
+
+@task
+async def get_docker_container_logs_flow(container_id):
+    # Export out the container logs
+    logger = get_run_logger()
+    logger.info(f"Logs from container with ID: {container_id}...")
+    container_logs = await get_docker_container_logs(container_id=container_id)
+    return container_logs
+
+
+@flow
+async def pull_and_run_image():
+    logger = get_run_logger()
+    logger.info("Loading Docker credentials...")
+    # Load Docker credentials
+    docker_registry_credentials = await DockerRegistryCredentials.load("seedoo-docker-registry")
+
+    # Pull the image
+    image = await pull_image(docker_registry_credentials)
+    logger.info(f"Pulled image: {image}")
+
+    # Create the container
+    container = await create_container()
+    logger.info(f"Created container: {container}")
 
     # Start the container
-    started_container = await start_docker_container(container_id=container.id)
-    print(f"Started container: {started_container}")
+    started_container = await start_container(container.id)
+    logger.info(f"Started container: {started_container}")
 
+    # Get the container logs
+    await asyncio.sleep(5)  # Add a 5 second sleep
+    container_logs = await get_docker_container_logs_flow(container.id)
+    logger.info(f"Container logs: {container_logs}")
+    
 if __name__ == "__main__":
     asyncio.run(pull_and_run_image())
