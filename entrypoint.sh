c1e5f1720026b0d01c2d77323b0471329ea271a8
Dijar Vrella <dijar.vrella@regionstockholm.se>
2024-06-25 16:55:49 +0200

    refactor: Update entrypoint.sh to use custom worker pool name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [ "$PREFECT_NODE_TYPE" == "server" ]; then
     echo "Starting Prefect server..."
     prefect server start
 elif [ "$PREFECT_NODE_TYPE" == "agent" ]; then
     echo "Starting Prefect agent..."
-    prefect worker start --pool seedoo-container-worker
+    prefect worker start --pool seedoo-custom-worker
 else
     echo "Unknown or unspecified PREFECT_NODE_TYPE: $PREFECT_NODE_TYPE"
     exec "$@"
