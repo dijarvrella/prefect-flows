c6551382ff20f8535616f697ca3f0bc9caea4a2e
Dijar Vrella <dijar.vrella@regionstockholm.se>
2024-07-04 23:58:08 +0200

    `refactor: Update compose.yaml to include prefect-worker service`

--- start-prefect-server.sh
+++ start-prefect-server.sh
@@ -14,14 +14,21 @@ check_http_response() {
   fi
 }
 # Check if the prefect-server container is running on the remote instance
+echo "Checking if Prefect server container is running on $REMOTE_HOST..."
 if ssh "$REMOTE_HOST" 'docker compose ls --filter "NAME=marvin"'; then
-  echo "Prefect-server container is running on $REMOTE_HOST."
+  echo " =========================================================== "
+  echo "  [OK] Prefect-server container is running on $REMOTE_HOST.  "
+  echo " =========================================================== "
 
   # Check HTTP response from Prefect server
   if check_http_response "http://172.172.233.164:4200/"; then
-    echo "Prefect server is running and responding."
+    echo " ================================================ "
+    echo "  [OK] Prefect server is running and responding.  "
+    echo " ================================================ "
   else
-    echo "Prefect server container is running but not responding properly."
+    echo " ================================================================== "
+    echo "  Prefect server container is running but not responding properly.  "
+    echo " ================================================================== "
     # Remote shell into the container on the remote instance and start the prefect server
     ssh "$REMOTE_HOST" "echo $COMPOSE_PATH"
     ssh "$REMOTE_HOST" "cd $COMPOSE_PATH && docker compose down && docker compose up -d"
