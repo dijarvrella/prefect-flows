523e2d651b7c8869736e3889c73317c86552977d
Dijar Vrella <dijar.vrella@regionstockholm.se>
2024-07-04 23:36:16 +0200

    `refactor: Update entrypoint.sh to start Prefect worker if PREFECT_NODE_TYPE is "worker"`

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ if [ "$PREFECT_NODE_TYPE" == "server" ]; then
     source /opt/prefect/entrypoint.sh
 elif [ "$PREFECT_NODE_TYPE" == "worker" ]; then
     echo "Starting Prefect worker..."
+    source /opt/prefect/entrypoint.sh
     prefect worker start --pool seedoo-custom-worker
 else
     echo "Unknown or unspecified PREFECT_NODE_TYPE: $PREFECT_NODE_TYPE"
