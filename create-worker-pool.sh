ef24f8be113b8f4d609f58b4c10b202e3fd1aca0
Dijar Vrella <dijar.vrella@regionstockholm.se>
2024-07-05 12:36:36 +0200

    `refactor: Update compose.yaml to include docker volume for /var/run/docker.sock`

---
+++ create-worker-pool.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Path to your custom base job template
+BASE_JOB_TEMPLATE_PATH="my-custom-base-job-template.yaml"
+
+# Work pool name
+WORK_POOL_NAME="seedoo-custom-worker-1"
+
+# Work pool type (e.g., kubernetes)
+WORK_POOL_TYPE="docker"
+
+# Create the work pool with the custom base job template
+prefect work-pool create "$WORK_POOL_NAME" --type "$WORK_POOL_TYPE" --base-job-template "$BASE_JOB_TEMPLATE_PATH"
