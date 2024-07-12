8515be707728450ecaf729e661756d5d50beb60e
Dijar Vrella <dijar.vrella@regionstockholm.se>
2024-07-12 23:50:59 +0200

    `refactor: Update Dockerfile and compose.yaml to use seedooinsights/prefect-worker image`

--- build-push-docker.sh
+++ build-push-docker.sh
@@ -2,12 +2,12 @@
 
 docker build -t seedoo-prefect-base --no-cache -f Dockerfile .
 
-docker tag seedoo-prefect-base:latest seedooinsights/prefect-agent:$1
-docker tag seedoo-prefect-base:latest seedooinsights/prefect-agent:latest
+docker tag seedoo-prefect-base:latest seedooinsights/prefect-worker:$1
+docker tag seedoo-prefect-base:latest seedooinsights/prefect-worker:latest
 docker tag seedoo-prefect-base:latest seedooinsights/prefect-base:$2
 docker tag seedoo-prefect-base:latest seedooinsights/prefect-base:latest
 
-docker push seedooinsights/prefect-agent:$1
-docker push seedooinsights/prefect-agent:latest
+docker push seedooinsights/prefect-worker:$1
+docker push seedooinsights/prefect-worker:latest
 docker push seedooinsights/prefect-base:$2
 docker push seedooinsights/prefect-base:latest
